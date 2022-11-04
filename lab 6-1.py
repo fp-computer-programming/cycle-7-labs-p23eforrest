708f454e9bb77c1497e80fcbe6dcbc030c24dea0
Ethan Forrest <p23eforrest@fairfieldprep.org>
2022-11-04 11:20:45 -0400

    labs

---
+++ lab 6-1.py
@@ -0,0 +1,11 @@
+# Author EAF 11/3/22
+my_arr = [1, 2, 3, 4, 5]
+print(my_arr[2])
+#creates variable that stores the list and prints the 3rd number in list
+print(len(my_arr))
+#prints the amount of numbers in the list
+print(my_arr * 3) 
+#prints the list if it were multiplied 3 times
+str = "string"
+print(list(str))
+#converts to the list and prints it as a string
---
+++ lab 6-2.py
@@ -0,0 +1,7 @@
+#Author: EAF 11/4/22
+my_row = ['Nic', 'Jack']
+my_row[1] = "Ethan"
+my_row2 = my_row
+print(my_row2)
+del my_row[1]
+print(my_row)
